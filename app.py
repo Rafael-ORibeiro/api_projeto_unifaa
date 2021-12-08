d979f6974ea3842a263bebdf98c020e47616c684
Rafael-ORibeiro <92262160+Rafael-ORibeiro@users.noreply.github.com>
2021-12-08 01:19:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.config['MONGO_URI'] = "mongodb+srv://ror74559:12mm34@betcartoes.jbbi4.mongodb.net/cadastro?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = ""
 
 #collection produtos
 mongo = PyMongo(app)
